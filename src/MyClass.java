c139b50ee4247ca5ebf928f87f0bdddf944dc3b6
Anca G <anca.gherman2013@gmail.com>
2022-12-12 16:54:30 +0200

    Versiunea 4

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,5 +2,9 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Ghit Hub");
         System.out.println("Versiunea 3");
+
+        System.out.println("Versiunea 4");
+
+        System.out.println("Versiunea 4");
     }
 }
